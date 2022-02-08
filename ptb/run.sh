84c162bbbb81498531c9269a2f6b9d4631dec0e4
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2022-02-08 12:33:06 -0700

    progress

--- ptb/run.sh
+++ ptb/run.sh
@@ -8,9 +8,9 @@ wget https://nlp.stanford.edu/software/stanford-parser-full-2013-11-12.zip
 unzip stanford-parser-full-2013-11-12.zip
 cp stanford-parser-full-2013-11-12/stanford-parser*.jar TreebankPreprocessing/
 # copy treebank to nltk's data directory
-PTB=/home/marco/repos/datasets/ptb/treebank_3_LDC99T42
-cp -R $PTB/treebank_3/parsed/mrg/wsj/ $HOME/nltk_data/corpora/ptb/WSJ
-cp -R $PTB/treebank_3/parsed/mrg/brown/ $HOME/marco/nltk_data/corpora/ptb/BROWN
+tar xzvf treebank_3_LDC99T42.tgz
+cp -R treebank_3/parsed/mrg/wsj/ $HOME/nltk_data/corpora/ptb/WSJ
+cp -R treebank_3/parsed/mrg/brown/ $HOME/nltk_data/corpora/ptb/BROWN
 # rename all files to uppercase
 find $HOME/nltk_data/corpora/ptb/WSJ -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\U$2/' {} \;
 find $HOME/nltk_data/corpora/ptb/BROWN -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\U$2/' {} \;
