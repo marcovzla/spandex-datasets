12df874c7a463a8048c5a36f1d1812f93b68f46e
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2022-02-08 17:46:39 -0700

    comments

--- ptb/run.sh
+++ ptb/run.sh
@@ -20,27 +20,24 @@ python ptb.py --output ../bracketed
 python ptb.py --task pos --output ../pos
 python tb_to_stanford.py --input ../bracketed --lang en --output ../conllx
 cd ..
-
+# convert constituency trees
 mkdir -p brat/bracketed/train
 mkdir -p brat/bracketed/dev
 mkdir -p brat/bracketed/test
-
 python bracketed2brat.py --inp bracketed/train.txt --out brat/bracketed/train
 python bracketed2brat.py --inp bracketed/dev.txt --out brat/bracketed/dev
 python bracketed2brat.py --inp bracketed/test.txt --out brat/bracketed/test
-
+# convert dependency trees
 mkdir -p brat/conllx/train
 mkdir -p brat/conllx/dev
 mkdir -p brat/conllx/test
-
 python conllXtostandoff.py -o brat/conllx/train conllx/train.conllx
 python conllXtostandoff.py -o brat/conllx/dev conllx/dev.conllx
 python conllXtostandoff.py -o brat/conllx/test conllx/test.conllx
-
+# convert pos tags
 mkdir -p brat/pos/train
 mkdir -p brat/pos/dev
 mkdir -p brat/pos/test
-
 python tsv2brat.py --inp pos/train.tsv --out brat/pos/train/
 python tsv2brat.py --inp pos/dev.tsv --out brat/pos/dev/
 python tsv2brat.py --inp pos/test.tsv --out brat/pos/test/
