05ef657fb891ee3e544514c4dbd3c9c27d8a7292
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2022-02-15 22:14:36 -0700

    deps

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pip install allennlp allennlp-models
+pip install allennlp allennlp-models matplotlib gensim
 # download other requirements
 python -m nltk.downloader ptb
 python -m spacy download en_core_web_sm
