7b194bb4963d71588051f2251f297fc56a09ce9c
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2022-02-15 22:52:57 -0700

    conda env

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # Datasets
-
-    conda create -n ds python=3.7
-    conda activate ds
-    ./setup.sh
--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
+eval "$(conda shell.bash hook)"
+conda create -n datasets3 python=3.7 -y
+conda activate datasets3
 pip install allennlp allennlp-models matplotlib gensim
-# download other requirements
 python -m nltk.downloader ptb
 python -m spacy download en_core_web_sm
+conda deactivate
