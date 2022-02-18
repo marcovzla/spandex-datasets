623ed6fe21cb9fd2f77a14f40a2172cedc021fdf
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2022-02-17 21:55:12 -0700

    conll ner 2003

---
+++ conll_ner_2003/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+git clone https://github.com/glample/tagger.git
+
+mkdir -p brat/train
+mkdir -p brat/dev
+mkdir -p brat/test
+
+eval "$(conda shell.bash hook)"
+conda activate datasets3
+
+# conll02tostandoff.py is adapted from https://github.com/nlplab/brat
+python conll02tostandoff.py -o brat/train tagger/dataset/eng.train
+python conll02tostandoff.py -o brat/dev tagger/dataset/eng.testa
+python conll02tostandoff.py -o brat/test tagger/dataset/eng.testb
+
+conda deactivate
